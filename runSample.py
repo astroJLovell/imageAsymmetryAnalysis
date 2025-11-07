61f96ab9f91a0d4aa8b7fa461c857af32318842f
astroJLovell <87023162+astroJLovell@users.noreply.github.com>
2025-11-07 15:30:05 -0500

    Update runSample.py
    locations specific to /ARKS/ project node in canfar

--- runSample.py
+++ runSample.py
@@ -1,1 +1,25 @@
-
+# run sample from command line; scripts needed per source
+import os
+###
+### In paper, for figs: C1 -- C5
+os.system('run rot_sub_cont.py /arc/projects/ARKS/data/products/HD9672/images/HD9672.combined.corrected.briggs.2.0.2048.0.01.fits')
+os.system('run rot_sub_cont.py /arc/projects/ARKS/data/products/HD10647/images/HD10647.combined.all.corrected.briggs.2.0.1024.0.03.fits')
+os.system('run rot_sub_cont.py /arc/projects/ARKS/data/products/HD15115/images/HD15115.combined.corrected.briggs.0.5.2048.0.01.fits')
+os.system('run rot_sub_cont.py /arc/projects/ARKS/data/products/HD32297/images/HD32297.combined.corrected.briggs.2.0.2048.0.005.fits')
+os.system('run rot_sub_cont.py /arc/projects/ARKS/data/products/HD39060/images/HD39060.combined.corrected.briggs.2.0.0.5arcsec.1024.0.03.fits')
+os.system('run rot_sub_cont.py /arc/projects/ARKS/data/products/HD61005/images/HD61005.combined.corrected.briggs.2.0.1024.0.01.fits')
+os.system('run rot_sub_cont.py /arc/projects/ARKS/data/products/HD92945/images/HD92945.combined.SMGsub.corrected.briggs.2.0.0.7arcsec.1024.0.04.fits')
+os.system('run rot_sub_cont.py /arc/home/JBLovell/asymmetries/continuum/HD107146/HD107146.b6.12m.SMGsub.corrected.briggs.2.0.1024.0.04.fits')
+os.system('run rot_sub_cont.py /arc/projects/ARKS/data/products/HD109573/images/HD109573.combined.corrected.briggs.0.5.2048.0.005.fits')
+os.system('run rot_sub_cont.py /arc/projects/ARKS/data/products/HD121617/images/HD121617.combined.corrected.briggs.0.5.1024.0.01.fits')
+# tentatives
+os.system('run rot_sub_cont.py /arc/projects/ARKS/data/products/HD84870/images/HD84870.combined.SMGsub.corrected.briggs.2.0.2048.0.04.fits')
+os.system('run rot_sub_cont.py /arc/projects/ARKS/data/products/HD95086/images/HD95086.combined.SMGsub.corrected.briggs.2.0.1.0arcsec.1024.0.04.fits')
+os.system('run rot_sub_cont.py /arc/projects/ARKS/data/products/HD131488/images/HD131488.combined.corrected.briggs.2.0.0.1arcsec.2048.0.003.fits')
+os.system('run rot_sub_cont.py /arc/projects/ARKS/data/products/HD131835/images/HD131835.combined.corrected.briggs.0.5.1024.0.01.fits')
+os.system('run rot_sub_cont.py /arc/projects/ARKS/data/products/HD218396/images/HD218396.combined.SMGsub.corrected.briggs.2.0.0.8arcsec.512.0.1.fits')
+###
+### Not presented in the paper, but of possible interest
+###
+os.system('run rot_sub_cont.py /arc/projects/ARKS/data/products/HD10647/images/HD10647.combined.arks.corrected.briggs.2.0.1024.0.03.fits')
+os.system('run rot_sub_cont.py /arc/projects/ARKS/data/products/HD10647/images/HD10647.combined.archival.corrected.briggs.2.0.1024.0.03.fits')
